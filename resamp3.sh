dd2ec8a22f56129746dc0d9fd214075853f28b8a
Chris Kimmel <kimmel.95@osu.edu>
2020-07-21 14:10:14 -0400

    Linting fixes to resamp3.sh

--- resamp3.sh
+++ resamp3.sh
@@ -1,5 +1,3 @@
-# resamp3.sh
-
 # This script copies a random sample of control fast5 files from one directory
 # into a temporary directory. It also copies all the experimental fast5s from
 # another temporary directory, then resquiggles both new directories (control
@@ -8,7 +6,7 @@
 # fast5s. Finally, it combines each of the two new fast5 directories into a
 # single file to prevent overuse of the file-number quota while preserving raw
 # data (just in case). The script does all this work in a new directory it
-# creates at the very beginning. There can be no spaces in any arguments.
+# creates at the very beginning.
 
 # This script takes four command-line arguments (must be run with "source")
 
@@ -29,14 +27,12 @@
 # that it will store all its data and results in. The current working directory
 # must not already contain a directory of this name.
 
-# Does the following "set -u" persist after conda changes the environment?
-set -u # Fail upon failed parameter expansion (i.e., undefined variable)
-
 conda activate tombo2 # Change conda environment name to match your own
-trap 'conda deactivate tombo2' EXIT # Ensures deactivate no matter what
+trap 'conda deactivate tombo2' EXIT # Ensures deactivation no matter what
 # Really it shouldn't matter that the environment deactivates, since this script
 # is intended to run as a qsub job
 
+set -u # Fail upon failed parameter expansion (i.e., undefined variable)
 set -x # Print every command to stdout before running it
 
 CTRL_ARG=$1
@@ -45,41 +41,42 @@ NUM_ARG=$3
 REF_ARG=$4
 WORK_DIR=$5
 
-if [ -e $WORK_DIR ]
+if [ -e "$WORK_DIR" ]
 then
     echo "Failure: WORK_DIR already exists:"
-    echo $WORK_DIR
+    echo "$WORK_DIR"
     exit 1
 fi
 
-mkdir $WORK_DIR
-mkdir ${WORK_DIR}/ctrl
-mkdir ${WORK_DIR}/exp
+mkdir "$WORK_DIR"
+mkdir "${WORK_DIR}"/ctrl
+mkdir "${WORK_DIR}"/exp
 
-# List files, one per line, unordered, from $EXP_ARG, and copy in chunks of 500
-ls -1U $EXP_ARG | \
-    xargs -n 500 -I '{}' cp -t $WORK_DIR/exp ${EXP_DIR}/'{}'
+# List files from $EXP_ARG and copy in chunks of 500
+find "$EXP_ARG" -maxdepth 1 -mindepth 1 -printf '%P\n' | \
+    xargs -n 500 -I '{}' cp -t "$WORK_DIR"/exp "${EXP_DIR}"/'{}'
 # Same, but randomly choose only NUM_ARG to copy
-ls -1U $CTRL_ARG | \
-    shuf -n $NUM_ARG | \
-    xargs -n 500 -I '{}' cp -t $WORK_DIR/ctrl ${CTRL_ARG}/'{}'
+find "$CTRL_ARG" -maxdepth 1 -mindepth 1 -printf '%P\n' | \
+    shuf -n "$NUM_ARG" | \
+    xargs -n 500 -I '{}' cp -t "$WORK_DIR"/ctrl "${CTRL_ARG}"/'{}'
 
-cd $WORK_DIR # We probably have to be in same directory as the fast5 basedirs
+# To be in same directory as fast5 basedirs
+cd "$WORK_DIR" || EXIT
 
-NUMPPROC=28 # Number of processes to use during Tombo resquiggling
+# I believe we need exp and ctrl to be resquiggled to corrected groups with the
+# same name, otherwise "tombo detect_modifications model_sample_compare" might
+# not compare the right corrected groups. (Tombo's choice of which corrected
+# group to read in the absence of the --corrected-group option is undocumented.)
+# That's why I added the --corrected-group and --overwrite options.
+#CORR_GRP
+NUM_PROC=28 # Number of processes to use during Tombo resquiggling
 for which in exp ctrl
 do
-    # I believe we need exp and ctrl to be resquiggled to corrected groups with
-    # the same name, otherwise "tombo detect_modifications model_sample_compare"
-    # might not compare the right corrected groups. (Tombo's choice of which
-    # corrected group to read in the absence of the --corrected-group option is
-    # undocumented.) That's why I added the --corrected-group and --overwrite
-    # options.
-    tombo resquiggle $which $REF_ARG \
-        --processes $NUMPROC \
+    tombo resquiggle "$which" "$REF_ARG" \
+        --processes "$NUM_PROC" \
         --q-score 7 \
-        --corrected-group resampling_experiment_resquiggle \
-        --overwrite \ # Just in case this script already processed these fast5s
+        --corrected-group "$CORR_GRP" \
+        --overwrite \
         --ignore-read-locks
 done
 
@@ -91,15 +88,16 @@ EXT='' # To help distinguish output files from runs with different parameters
 detect_modifications model_sample_compare \
     --fast5-basedirs exp \
     --control-fast5-basedirs ctrl \
-    --statistics-file-basename sf_${NUM_ARG}${EXT} \
-    --per-read-statistics-basename prs_${NUM_ARG}${EXT} \
+    --statistics-file-basename "sf_${NUM_ARG}${EXT}" \
+    --per-read-statistics-basename "prs_${NUM_ARG}${EXT}" \
     --sample-only-estimates \
-    --fishers-method-context $fisher_context \
-    --corrected-group $corr_grp
+    --fishers-method-context 3 \
+    --corrected-group "${CORR_GRP}"
 
 for which in exp ctrl
 do
     tar -cf ${which}.tar ${which}/*
+    rm -r $which
 done
 
 cd ..
