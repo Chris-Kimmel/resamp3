8905ca70941f675d587452f66f66050a3a221670
Chris Kimmel <kimmel.95@osu.edu>
2020-07-22 10:15:44 -0400

    Bugfix and cosmetic changes

--- qsub_submitter.sh
+++ qsub_submitter.sh
@@ -1,18 +1,16 @@
 CTRL_ARG=/fs/project/PAS1405/General/Kimmel_Chris/f1_ssfast5s_4500
 CTRL_ARG+=" /fs/project/PAS1405/General/Kimmel_Chris/f2_ssfast5s_4500"
 EXP_ARG=/users/PAS1405/kimmel/resampling_experiment_2/experimental
-NUM_ARG_LIST="20 20"
+NUM_ARG_LIST="20 30"
 REF_ARG=/fs/project/PAS1405/General/Kimmel_Chris/RNA_section__454_9627.fa
 WORK_ARG=/users/PAS1405/kimmel/resamp3
 
 for NUM_ARG in NUM_ARG_LIST:
 do
-    TEMP_NAME=${RANDOM}${RANDOM}
-    echo "cd /users/PAS1405/kimmel/resamp3" > ${TEMP_NAME}
+    JOB_FILE=${RANDOM}${RANDOM}
+    echo "cd /users/PAS1405/kimmel/resamp3" > ${JOB_FILE}
     echo "source resamp3.sh \"${CTRL_ARG}\" ${EXP_ARG} ${NUM_ARG} ${REF_ARG} ${WORK_ARG}" \
-        >> ${TEMP_NAME}
-    qsub -l walltime=2:00:00,nodes=1:ppn=28 -j oe ${TEMP_NAME} -N ${NUM_ARG}_${TEMP_NAME}
-    echo "rm $TEMP_NAME" # TODO: Remove echo and quotes
+        >> ${JOB_FILE}
+    qsub -l walltime=2:00:00,nodes=1:ppn=28 -j oe -N ${NUM_ARG}_${JOB_FILE} ${JOB_FILE}
+    echo "rm $JOB_FILE" # TODO: Remove echo and quotes
 done
-
-# TODO: Make the job name correlate with the directory name
