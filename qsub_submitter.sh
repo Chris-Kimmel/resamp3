099181003b5fbe4aecbb5c19397d5b889e13b018
Chris Kimmel <kimmel.95@osu.edu>
2020-07-29 08:56:46 -0400

    Change qsub submitter to submit jobs at control depths 10, 20, ... 90.

--- qsub_submitter.sh
+++ qsub_submitter.sh
@@ -4,8 +4,12 @@ EXP_ARG=/users/PAS1405/kimmel/resampling_experiment_2/experimental
 REF_ARG=/fs/project/PAS1405/General/Kimmel_Chris/RNA_section__454_9627.fa
 WORK_ARG=/users/PAS1405/kimmel/resamp3
 
-# The run with "100000 reads" really just takes every available read
-for num_arg in {100..900..100} {1000..9000..1000} {10000..50000..10000} 100000
+# NOTE. Originally I ran this script with minimum control depth 100.
+# ... later I decided I wanted results for 10, 20, ..., 90 too, but it wasn't
+# ... worth re-running the whole thing.
+# ALSO NOTE: The run with "100000 reads" really just takes every available read
+# for num_arg in {100..900..100} {1000..9000..1000} {10000..50000..10000} 100000
+for num_arg in {10..90..10}
 do
     RANDOM_ID=${RANDOM}${RANDOM} # Consider moving this outside the loop
     JOB_FILE=${num_arg}_${RANDOM_ID}
